63ea32b9721bf351e673be8e5ce3e5767b59914e
tadree <P@ssword1408>
2023-01-13 14:37:06 +0300

    Changed table check

--- src/test/java/ru/jiehk/StudentRegistrationFormTest.java
+++ src/test/java/ru/jiehk/StudentRegistrationFormTest.java
@@ -51,15 +51,15 @@ public class StudentRegistrationFormTest {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $("tr:nth-child(1) > td:nth-child(2)").shouldHave(text(firstName + ' ' + lastName));
-        $("tr:nth-child(2) > td:nth-child(2)").shouldHave(text(email));
-        $("tr:nth-child(3) > td:nth-child(2)").shouldHave(text(gender));
-        $("tr:nth-child(4) > td:nth-child(2)").shouldHave(text(mobile));
-        $("tr:nth-child(5) > td:nth-child(2)").shouldHave(text("19 January,1997"));
-        $("tr:nth-child(6) > td:nth-child(2)").shouldHave(text(subjects));
-        $("tr:nth-child(7) > td:nth-child(2)").shouldHave(text(hobbies));
-        $("tr:nth-child(8) > td:nth-child(2)").shouldHave(text("test.jpeg"));
-        $("tr:nth-child(9) > td:nth-child(2)").shouldHave(text(currentAddress));
-        $("tr:nth-child(10) > td:nth-child(2)").shouldHave(text(state + ' ' + city));
+        $(".table-responsive").shouldHave(text(firstName + ' ' + lastName),
+                text(email),
+                text(gender),
+                text(mobile),
+                text("19 January,1997"),
+                text(subjects),
+                text(hobbies),
+                text("test.jpeg"),
+                text(currentAddress),
+                text(state + ' ' + city));
     }
 }
